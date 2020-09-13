3cebda149ec49e2efeaa151bd13f104fdc0ed71e
Timur Myngbay <mtima97@gmail.com>
2020-09-14 00:50:24 +0600

    Fix bug

--- app/Product.php
+++ app/Product.php
@@ -8,6 +8,8 @@ class Product extends Model
 {
     protected $table = 'products';
 
+    protected $fillable = ['name', 'color', 'weight', 'price'];
+
     public function categories()
     {
         return $this->belongsToMany(Category::class, 'category_product', 'product_id', 'category_id')
