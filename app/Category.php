1e867c8b410376e28c20a1fd477a60523545165a
Timur Myngbay <mtima97@gmail.com>
2020-09-13 22:08:53 +0600

    Add fillable

--- app/Category.php
+++ app/Category.php
@@ -8,6 +8,8 @@ class Category extends Model
 {
     protected $table = 'categories';
 
+    protected $fillable = ['name'];
+
     public function products()
     {
         return $this->belongsToMany(Product::class, 'category_product', 'category_id', 'product_id')
