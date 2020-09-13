2752bc105ce74e71fa32eb2673de8f28f4f6e426
Timur Myngbay <mtima97@gmail.com>
2020-09-13 22:17:45 +0600

    Category routes

--- routes/api.php
+++ routes/api.php
@@ -1,25 +1,21 @@
 <?php
 
-use Illuminate\Http\Request;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
-Route::group(['middleware' => 'api', 'prefix' => 'auth', 'namespace' => 'Api', 'as' => 'authApi.'], function () {
+Route::group(['prefix' => 'auth', 'namespace' => 'Api', 'as' => 'authApi.'], function () {
     Route::post('login', 'AuthController@login')->name('login');
     Route::post('logout', 'AuthController@logout')->name('logout');
     Route::post('refresh', 'AuthController@refresh')->name('refresh');
     Route::post('me', 'AuthController@me')->name('me');
+});
+
+Route::group([
+    'prefix' => 'categories',
+    'namespace' => 'Api',
+    'as' => 'categories.',
+    'middleware' => 'auth:api'
+], function () {
+    Route::get('/', 'CategoryController@index')->name('all');
+    Route::post('/', 'CategoryController@add')->name('add');
+    Route::get('/{category}/products', 'CategoryController@products')->name('products');
+    Route::post('/{category}/update', 'CategoryController@update')->name('update');
+    Route::post('/{category}/delete', 'CategoryController@delete')->name('delete');
 });
